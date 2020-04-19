70832dae7b31909f1bee1c8f44d5d7731fe8406b
Lim Chun Hoong <chunhoong.lim@outlook.my>
2020-04-19 15:21:44 +0800

    Handle GET request bean validation

--- src/main/java/com/chunhoong/featureaccessservice/exception/ExceptionHandler.java
+++ src/main/java/com/chunhoong/featureaccessservice/exception/ExceptionHandler.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,8 +21,27 @@ public class ExceptionHandler {
   private static Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
 
   @org.springframework.web.bind.annotation.ExceptionHandler({ MethodArgumentNotValidException.class })
-  protected ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex, WebRequest request) {
+  protected ResponseEntity<Object> handleValidationException(MethodArgumentNotValidException ex, WebRequest request) {
     logger.error("MethodArgumentNotValidException: ", ex);
+
+    Map<String, Object> body = new LinkedHashMap<>();
+    body.put("timestamp", LocalDateTime.now());
+    body.put("status", HttpStatus.BAD_REQUEST.value());
+    // @formatter:off
+    body.put("errors", ex.getBindingResult()
+      .getFieldErrors()
+      .stream()
+      .map(FieldError::getDefaultMessage)
+      .collect(Collectors.toList())
+    );
+    // @formatter:on
+
+    return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+  }
+
+  @org.springframework.web.bind.annotation.ExceptionHandler({ BindException.class })
+  protected ResponseEntity<Object> handleBindException(BindException ex, WebRequest request) {
+    logger.error("BindException: ", ex);
 
     Map<String, Object> body = new LinkedHashMap<>();
     body.put("timestamp", LocalDateTime.now());
